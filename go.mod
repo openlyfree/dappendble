907aabbe3c672ddaa03ac9e7a31de4bab1952e8f
Ethan J. <duhxu0@gmail.com>
2026-04-07 23:52:52 -0700

    rename to dappendble

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/openlyfree/teensy-store
+module github.com/openlyfree/dappendble
 
 go 1.26.1
 
--- internal/engine/table.go
+++ internal/engine/table.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 	"sync"
 
-	"github.com/openlyfree/teensy-store/internal/models"
+	"github.com/openlyfree/dappendble/internal/models"
 	"google.golang.org/protobuf/proto"
 )
 
